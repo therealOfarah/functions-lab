1e44f3197435e6d6c76ddbfa66d643cec11df684
Omar Farah <omaarfaarah2019@gmail.com>
2022-08-02 07:47:29 -0400

    complete lab

---
+++ python-functions.py
@@ -0,0 +1,27 @@
+# task 1
+# def sum_to(n):
+#   total = 0
+#   for x in range(1,n +1):
+#     total += x 
+#     print(total)
+# print(sum_to(10))
+
+# task 2
+# def largest(a,b,c,d):
+#   x= max([a,b,c,d])
+#   print(x)
+
+# print(largest(10,20,90,40))
+
+# def occurrences(a,b):
+#   print(a.count(b))
+# occurrences("cheese",'e')
+
+# task4
+
+# def products(*args):
+#   total = 1
+#   for num in args:
+#     total *=num 
+#   print(total)
+# products(-1,4)
